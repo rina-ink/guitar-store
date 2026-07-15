c74efda2a2d74866c10cc2a0fca93a029d6ee710
rina-ink <dominkovicmarina@gmail.com>
2026-07-15 19:37:27 +0200

    connect MongoDB with Mongoose

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,16 @@
 import express from "express";
+import cors from "cors";
+import connectDb from "./db/index.ts";
 
 const app = express();
 
 const PORT = process.env.PORT || 8080;
 
+connectDb();
+
 // middleware
-app.use(express.json());
+app.use(express.json());  
+app.use(cors());   // allows other websites to make requests to the API
 
 // routes
 app.get("/", (req, res) => {
@@ -15,3 +20,15 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
     console.log(`Guitar Store API listening at http://localhost:${PORT}`);
 });
+
+
+/* cors
+think of it like a bouncer
+Imagine the API is a club
+
+without cors: frontend -> API -> You're not on the guest list
+with cors: frontend -> API -> Come in
+
+.
+
+each middleware gets a chance to inspect or modify the request/response before it reaches your controller */
